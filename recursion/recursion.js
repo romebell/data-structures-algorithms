073655177763f805c264329930131b8485e483dd
Rome Bell <romebell@me.com>
2019-07-23 14:02:29 -0700

    Add fibonacci sequence method

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -11,3 +11,21 @@ function countdown(n) {
 }
 
 countdown(10);
+
+// Factorial
+function factorial(n) {
+  if (n === 1) return 1;
+  
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+// Fibonacci Squaure
+function fib(n) {
+  if (n === 1 || n === 2) return 1;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(7));
